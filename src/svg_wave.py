89ed5627e8a42387607c57856cafcbda23cb53ea
21jon <acc@jonaslbgtt.de>
2024-06-05 17:04:27 +0200

    ich wende den toaster badewannen glitch gleich an

--- src/svg_wave.py
+++ src/svg_wave.py
@@ -1,21 +1,44 @@
 import os 
 import util.writer as writer
+import sys
 
 # create a wave form svg animaiton
 def create_wave():
+    print("Creating wave svg") 
     ani_index = 0
-    for j in range(0,2):
-        for i in range(290,0):
+    for i in range(0,290,8):
+        writer.write_file(f"../output/wave_{ani_index}.svg",
+        f"""<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" >
+        <path d="M0 100 A100 {i}  0 0 1 100 100" fill="none" stroke="white" />
+        <path d="M100 100 A100 {i} 0 0 0 200 100" fill="none" stroke="#ffffff" />
+        </svg>""" )
+        ani_index += 1
+    
+    for i in range(0,290, 8)[::-1]:
+        writer.write_file(f"../output/wave_{ani_index}.svg",
+        f"""<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" >
+        <path d="M0 100 A100 {i}  0 0 1 100 100" fill="none" stroke="white" />
+        <path d="M100 100 A100 {i} 0 0 0 200 100" fill="none" stroke="#ffffff" />
+        </svg>""" )
+        ani_index += 1
+    
+    
+    for i in range(290,0,8):
             writer.write_file(f"../output/wave_{ani_index}.svg",
-    f"""<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" >
-    <path d="M0 100 A100 {i}  0 0 {1 if j == 0 else 0} 100 100" fill="none" stroke="white" />
-    <path d="M100 100 A100 {i} 0 0 {j} 200 100" fill="none" stroke="#ffffff" />
-</svg>""" )
+            f"""<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" >
+            <path d="M0 100 A100 {i}  0 0 0 100 100" fill="none" stroke="white" />
+            <path d="M100 100 A100 {i} 0 0 1 200 100" fill="none" stroke="#ffffff" />
+            </svg>""" )
             ani_index += 1
-            
+
+    
+    for i in range(290,0,8)[::-1]:
+            writer.write_file(f"../output/wave_{ani_index}.svg",
+            f"""<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" >
+            <path d="M0 100 A100 {i}  0 0 0 100 100" fill="none" stroke="white" />
+            <path d="M100 100 A100 {i} 0 0 1 200 100" fill="none" stroke="#ffffff" />
+            </svg>""" )
+            ani_index += 1
 
 
-
-
-if __name__ == "__main__":
-    create_wave()
+create_wave()
